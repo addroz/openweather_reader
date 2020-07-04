154156a2cc6329a43c01c4aaed0094ba6ab47058
Adam Drożyński <adam.drozynski2@gmail.com>
2020-07-04 13:47:24 +0200

    Dalsze zmiany

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,71 @@
 import React from 'react';
 import './App.css';
-import { CitySelect } from './features/citySelect/citySelect.js';
+import {CitySelect} from './features/citySelect/citySelect.js';
+import {createStore} from 'redux'
+const apiKey = '2f43081da337fcebfc53e06c931c16a5';
+
+const MODE = {
+    LIGHT: "light",
+    DARK: "dark"
+}
+
+const initialSelection = {
+    cityId: -1,
+    mode: MODE.LIGHT
+}
+
+function changeMode(state) {
+    if (state.mode === MODE.LIGHT) {
+        return {
+            ...state, mode: MODE.DARK
+        }
+    } else {
+        return {
+            ...state, mode: MODE.LIGHT
+        }
+    }
+}
+
+function counter(state = initialSelection, action) {
+    if (action.type === 'MODE') {
+        return changeMode(state)
+    }
+
+    if (action.type === 'SELECT') {
+        return {
+            ...state, cityId: action.id
+        }
+    }
+
+    return state
+}
+
+function getWeather(cityId) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&appid=' + apiKey)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+}
+
+const store = createStore(counter)
+document.store = store
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <CitySelect />
-      </header>
-      <footer>
-          Author: Adam Drożyński, 395133
-      </footer>
-    </div>
-  );
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CitySelect/>
+            </header>
+            <footer>
+                Author: Adam Drożyński, 395133
+            </footer>
+        </div>
+      );
 }
 
 export default App;
--- my-app/src/features/citySelect/citySelect.js
+++ my-app/src/features/citySelect/citySelect.js
@@ -1,16 +1,55 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, {Component} from 'react';
+import data from './city.list.json'
+import ReactAutocomplete from 'react-autocomplete'
 
-export function CitySelect() {
-    return (
-        <div>
+function changeSelectedCity(id) {
+    document.store.dispatch({type: 'SELECT', id: id})
+}
+
+export class CitySelect extends Component {
+    cityList = data.map((e) => {
+        return (
+            {id: e.id, label: e.name}
+        )
+    });
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            value: '',
+        }
+    }
+
+    render() {
+        return (
             <div>
-                <input type="text" aria-label="Select" >
-                </input>
-                <button aria-label="Select" >
-                    Select
-                </button>
+                <div>
+                    <ReactAutocomplete
+                        items={this.cityList}
+                        shouldItemRender={(item, value) => value.length > 3 &&
+                            item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                        getItemValue={item => item.id}
+                        renderItem={(item, highlighted) =>
+                            <div
+                                key={item.id}
+                                style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                            >
+                                {item.label}
+                            </div>
+                        }
+                        value={this.state.value}
+                        onChange={e => this.setState({ value: e.target.value })}
+                        onSelect={value => {
+                            console.log(value)
+                            changeSelectedCity(value)
+                            this.setState({value})
+                        }}
+                    />
+                    <button aria-label="Select" >
+                        Select
+                    </button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
